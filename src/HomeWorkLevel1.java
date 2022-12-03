a33b639b60fd1b6e63dce7e87b0ff919335840ca
par55 <par555@mail.ru>
2022-12-03 14:39:37 +0100

    Initial commit

---
+++ src/HomeWorkLevel1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HomeWorkLevel1 {
+    public static void main(String[] args) {
+    Scanner box = new Scanner(System.in);
+    int dollars = box.nextInt();
+    if (dollars == 0) {
+        System.out.println("ошибок не обнаружено");
+    }
+    else {
+        System.out.println("ERROR");
+        System.out.println("ERROR");
+        System.out.println("ERROR");
+    }
+    }
+}
